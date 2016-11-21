69c3d4ce442a2e768031222318046ac5a3cad379
Henri Tremblay <henri.tremblay@gmail.com>
2016-11-21 13:09:40 -0500

    Initial commit

---
+++ lib/src/main/java/pro/tremblay/MyCode.java
@@ -0,0 +1,13 @@
+package pro.tremblay;
+
+/**
+ * Hello world!
+ *
+ */
+public class MyCode
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ main/src/main/java/pro/tremblay/App.java
@@ -0,0 +1,13 @@
+package pro.tremblay;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        MyCode.main(args);
+    }
+}
